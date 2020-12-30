9faa0e6f037b141fbdf7c00add4f938a7a2cbf94
cobracommanda <drobinson@benchmarkeducation.com>
2020-12-29 19:33:04 -0500

    004-8-blueprint-ui-window

--- Modules/System/blueprint_UI.py
+++ Modules/System/blueprint_UI.py
@@ -1,3 +1,18 @@
+import maya.cmds as cmds
+
 class Blueprint_UI:
     def __init__(self):
-        print "We are in the constructor"
+        # Store UI Element in a dictionary
+        self.UI_elements = {}
+        
+        if cmds.window("blueprint_UI_window", exists=True):
+            cmds.deleteUI("blueprint_UI_window")
+
+        window_width = 400
+        window_height = 590
+
+        self.UI_elements["window"] = cmds.window("blueprint_UI_window", width=window_width, height=window_height, title="Blueprint Module UI", sizeable=False)
+
+
+        # Display window
+        cmds.showWindow(self.UI_elements["window"])
